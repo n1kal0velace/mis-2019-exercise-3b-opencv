410c1be77e14af89aa05fb7c11e50838ec1024df
sharadbrat <sharoobrazno@mail.ru>
2019-05-04 17:10:00 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+
+# OpenCV + Android assignment
+
+To import opencv library into the android without OpenCV Manager, we used the following tutorial:
+
+** https://android.jlelse.eu/a-beginners-guide-to-setting-up-opencv-android-library-on-android-studio-19794e220f3c **
+
---
+++ app/src/main/java/com/nika_georgii/opencvassignment/MainActivity.java
@@ -0,0 +1,13 @@
+package com.nika_georgii.opencvassignment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
